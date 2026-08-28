1d2d9699b99740957d3a030117d6a1fadc4828fd
kagishogailele9 <kagishogailele9@gmail.com>
2026-08-29 01:23:24 +0200

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,18 @@
+{
+  "background_color": "#030712",
+  "dir": "ltr",
+  "display": "standalone",
+  "name": "kagishogailele9.github",
+  "orientation": "portrait",
+  "scope": "./",
+  "short_name": "kagishogailele9.github",
+  "start_url": "./",
+  "theme_color": "#4f46e5",
+  "id": "./",
+  "description": "A modern task management application",
+  "lang": "en",
+  "prefer_related_applications": false,
+  "categories": [
+    "productivity"
+  ]
+}
